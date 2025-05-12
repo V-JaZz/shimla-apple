8b4c1a91c28f69e09bb8dea1dd182dd03dad33ac
Abhishek <abhibhardwaj1017@gmail.com>
2025-05-13 00:11:52 +0530

    Feat: Home hero component

---
+++ src/app/(home)/_components/hero.tsx
@@ -0,0 +1,40 @@
+import classes from "./hero.module.css";
+import { Box, Button, Container, Flex, Group, Overlay, Text, Title } from "@mantine/core";
+import Link from "next/link";
+
+export default function Hero() {
+    return (
+        <Box component="section" className={classes.hero}>
+            <Flex align="center" className={classes.wrapper}>
+                <Overlay color="#000" opacity={0.75} zIndex={1} />
+
+                <Container size="xl">
+                    <Flex direction="column" align="center" className={classes.inner}>
+                        <Title c="white" ta="center">
+                            Fresh from the Hills of Shimla
+                        </Title>
+
+                        <Text maw={600} ta="center" c="white" mt={15}>
+                            Experience the crisp, sweet taste of premium Shimla apples,
+                            handpicked from our orchards
+                        </Text>
+
+                        <Group mx="auto" mt={20}>
+                            <Button size="md" component={Link} href="/products">
+                                Shopw Now
+                            </Button>
+                            <Button
+                                size="md"
+                                variant="white"
+                                component={Link}
+                                href="/about"
+                            >
+                                Learn More
+                            </Button>
+                        </Group>
+                    </Flex>
+                </Container>
+            </Flex>
+        </Box>
+    )
+}
